85de1493ed030359a959e9f7d0076fd85a49d6ad
ey.wang <wangenyong@foxmail.com>
2021-09-27 14:01:24 +0800

    修改F2 chart

--- src/index.js
+++ src/index.js
@@ -1,25 +1,46 @@
-import F2 from '@antv/f2';
-// 第一步：加载插件 ScrollBar
-const ScrollBar = require('@antv/f2/lib/plugin/scroll-bar');
-// 第二步：注册插件 ScrollBar
-F2.Chart.plugins.register(ScrollBar); // 这里进行全局注册，也可以给 chart 的实例注册
+// f2-canvas.js
+import Renderer from './lib/renderer';
+import F2 from './lib/f2';
 
-function wrapEvent(e) {
-  if (!e) return;
-  if (!e.preventDefault) {
-    e.preventDefault = function () {};
+// 适配小程序的事件机制
+F2.Util.addEventListener = function (source, type, listener) {
+  source.addListener(type, listener);
+};
+
+F2.Util.removeEventListener = function (source, type, listener) {
+  source.removeListener(type, listener);
+};
+
+F2.Util.createEvent = function (event, chart) {
+  const type = event.type;
+  let x = 0;
+  let y = 0;
+  const touches = event.touches;
+  if (touches && touches.length > 0) {
+    x = touches[0].x;
+    y = touches[0].y;
   }
-  return e;
-}
+
+  return {
+    type,
+    chart,
+    x,
+    y
+  };
+};
 
 Component({
   /**
    * 组件的属性列表
    */
   properties: {
-    onInit: {
-      type: 'Function',
-      value: () => {}
+    canvasId: {
+      type: String,
+      value: 'f2-canvas'
+    },
+
+    opts: {
+      type: Object
     }
   },
 
@@ -30,70 +51,63 @@ Component({
 
   },
 
-  ready() {
-    const query = wx.createSelectorQuery().in(this);
-    query.select('.f2-canvas')
-      .fields({
-        node: true,
-        size: true
-      })
-      .exec(res => {
-        const {
-          node,
-          width,
-          height
-        } = res[0];
-        const context = node.getContext('2d');
-        const pixelRatio = wx.getSystemInfoSync().pixelRatio;
-        // 高清设置
-        node.width = width * pixelRatio;
-        node.height = height * pixelRatio;
+  ready: function () {
+    if (!this.data.opts) {
+      console.warn('组件需绑定 opts 变量，例：<ff-canvas id="mychart-dom-bar" ' +
+        'canvas-id="mychart-bar" opts="{{ opts }}"></ff-canvas>');
+      return;
+    }
 
-        const config = {
-          context,
-          width,
-          height,
-          pixelRatio
-        };
-        const chart = this.data.onInit(F2, config);
-        if (chart) {
-          this.chart = chart;
-          this.canvasEl = chart.get('el');
-          this.triggerEvent('on-chart-ready-event', {})
-        }
-      });
+    if (!this.data.opts.lazyLoad) {
+      this.init();
+    }
   },
 
   /**
    * 组件的方法列表
    */
   methods: {
-    touchStart(e) {
-      const canvasEl = this.canvasEl;
-      if (!canvasEl) {
+    init: function (callback) {
+      const version = wx.version.version.split('.').map(n => parseInt(n, 10));
+      const isValid = version[0] > 1 || (version[0] === 1 && version[1] > 9) ||
+        (version[0] === 1 && version[1] === 9 && version[2] >= 91);
+      if (!isValid) {
+        console.error('微信基础库版本过低，需大于等于 1.9.91。');
         return;
       }
-      canvasEl.dispatchEvent('touchstart', wrapEvent(e));
+
+      const ctx = wx.createCanvasContext(this.data.canvasId, this); // 获取小程序上下文
+      const canvas = new Renderer(ctx);
+      this.canvas = canvas;
+
+      const query = wx.createSelectorQuery().in(this);
+      query.select('.f2-canvas').boundingClientRect(res => {
+        if (typeof callback === 'function') {
+          this.chart = callback(F2, canvas, res.width, res.height);
+        } else if (this.data.opts && this.data.opts.onInit) {
+          this.chart = this.data.opts.onInit(F2, canvas, res.width, res.height);
+        }
+      }).exec();
+    },
+    touchStart(e) {
+      if (this.canvas) {
+        this.canvas.emitEvent('touchstart', [e]);
+      }
     },
     touchMove(e) {
-      const canvasEl = this.canvasEl;
-      if (!canvasEl) {
-        return;
+      if (this.canvas) {
+        this.canvas.emitEvent('touchmove', [e]);
       }
-      canvasEl.dispatchEvent('touchmove', wrapEvent(e));
     },
     touchEnd(e) {
-      const canvasEl = this.canvasEl;
-      if (!canvasEl) {
-        return;
+      if (this.canvas) {
+        this.canvas.emitEvent('touchend', [e]);
       }
-      canvasEl.dispatchEvent('touchend', wrapEvent(e));
     },
-    /**
-     * 图表数据更新（前后数据结构不发生变化），需要马上更新图表。
-     */
-    changeData(data) {
-      this.chart.changeData(data);
-    },
+    press(e) {
+      if (this.canvas) {
+        this.canvas.emitEvent('press', [e]);
+      }
+    }
   }
-});
+})
